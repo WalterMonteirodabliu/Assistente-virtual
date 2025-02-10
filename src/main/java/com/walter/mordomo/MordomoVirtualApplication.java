26192e5b19de4e0d89066f943712d7455ec54e64
WalterMonteirodabliu <walter.felipemonteiro@gmail.com>
2025-02-10 14:32:37 -0300

    Initial commit

---
+++ src/main/java/com/walter/mordomo/MordomoVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.walter.mordomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MordomoVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MordomoVirtualApplication.class, args);
+	}
+
+}
